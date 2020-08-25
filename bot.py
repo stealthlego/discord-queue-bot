0372a6f14befacb249ed363f16e6e1b1da86a22d
stealthlego <walexanderanderson@gmail.com>
2020-08-25 08:55:31 -0700

    Working embed and filtering

--- bot.py
+++ bot.py
@@ -2,28 +2,57 @@
 
 import os
 
+import discord
 from discord.ext import commands
 from dotenv import load_dotenv
+from datetime import datetime
 import sports
 
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
 #GUILD = os.getenv('DISCORD_GUILD')
 
-#client = discord.Client()
+client = discord.Client()
 bot = commands.Bot(command_prefix='?')
 
 @bot.command(name='today', help='Responds if there is a hockey game today')
 async def sport_today(ctx, sport):
     #match = sports.get_match(sports.HOCKEY, 'islanders', 'flyers')
 
-    await ctx.send('Let me check...')
+    checking_msg = await ctx.send('Let me check...')
 
-    all_matches = sports.all_matches()
-    hockey = all_matches[sport]
-    first_game = hockey[0]
-    #print(response)
-    await ctx.send(f'{first_game.home_team}: {first_game.home_score}\n{first_game.away_team}: {first_game.away_score}')
+    #need to parse input for specific sport
+
+    all_matches = sports.get_sport(sports.HOCKEY)
+    match_list = []
+    sorting_msg = await ctx.send(f'Sorting through {len(all_matches)} matches...')
+    for match in all_matches:
+        #print(f'Match day: {match.match_date.day}')
+        #print(f'Date day: {datetime.today().day}')
+        #print(str(match.league))
+        if match.match_date.day == datetime.today().day and 'USA' in match.league:
+            match_list.append(match)
+
+    embed = discord.Embed(
+        title = 'Games Today',
+        description = f'List of {sport} games today',
+        color = discord.Color.blue()
+    )
+
+    for game in match_list:
+        time_string = game.match_date.strftime('%H:%M')
+        embed.add_field(
+            name=f'Game {match_list.index(game)+1} @ {time_string}', 
+            value=f'{game.home_team} V {game.away_team}',
+            inline=False
+        )
+
+    #paste embed
+    await ctx.send(embed=embed)
+    #cleanup
+    await checking_msg.delete()
+    await sorting_msg.delete()
+
 
 '''
 @client.event
