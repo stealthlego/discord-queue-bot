296ed6a6b948bc5dc559169fd8c6eee1ff37549e
stealthlego <walexanderanderson@gmail.com>
2020-09-05 18:49:40 -0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -5,8 +5,6 @@ import os
 import discord
 from discord.ext import commands
 from dotenv import load_dotenv
-from datetime import datetime
-import sports
 
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
@@ -15,44 +13,18 @@ TOKEN = os.getenv('DISCORD_TOKEN')
 client = discord.Client()
 bot = commands.Bot(command_prefix='?')
 
-@bot.command(name='today', help='Responds if there is a hockey game today')
+@bot.command(name='create', help='Responds if there is a hockey game today')
 async def sport_today(ctx, sport):
     #match = sports.get_match(sports.HOCKEY, 'islanders', 'flyers')
 
-    checking_msg = await ctx.send('Let me check...')
+    checking_msg = await ctx.send('Assembling queue...')
 
-    #need to parse input for specific sport
+    #create queue
+    name_string = '@StealthLego'
 
-    all_matches = sports.get_sport(sports.HOCKEY)
-    match_list = []
-    sorting_msg = await ctx.send(f'Sorting through {len(all_matches)} matches...')
-    for match in all_matches:
-        #print(f'Match day: {match.match_date.day}')
-        #print(f'Date day: {datetime.today().day}')
-        #print(str(match.league))
-        if match.match_date.day == datetime.today().day and 'USA' in match.league:
-            match_list.append(match)
-
-    embed = discord.Embed(
-        title = 'Games Today',
-        description = f'List of {sport} games today',
-        color = discord.Color.blue()
-    )
-
-    for game in match_list:
-        time_string = game.match_date.strftime('%H:%M')
-        embed.add_field(
-            name=f'Game {match_list.index(game)+1} @ {time_string}', 
-            value=f'{game.home_team} V {game.away_team}',
-            inline=False
-        )
-
-    #paste embed
-    await ctx.send(embed=embed)
+    await ctx.send(f'Queue Create! First up is {name_string}')
     #cleanup
     await checking_msg.delete()
-    await sorting_msg.delete()
-
 
 '''
 @client.event
