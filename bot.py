9ec682cf3bb594f6445b6b434ce0afaf620a8e09
stealthlego <walexanderanderson@gmail.com>
2020-10-07 11:58:26 -0700

    redesigned bot structure to work with many servers

--- bot.py
+++ bot.py
@@ -1,9 +1,10 @@
-#Queue Bot
+#QueueBot
 #Author: Alex Anderson
 
 import os
 import random
 import time
+import datetime
 
 import discord
 from discord.ext import commands, tasks
@@ -12,25 +13,21 @@ from dotenv import load_dotenv
 
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
-#GUILD = os.getenv('DISCORD_GUILD')
 
-client = discord.Client()
-bot = commands.Bot(command_prefix='?')
+bot = commands.Bot(command_prefix='{')
 
-#user_list = []
-#server_handler = {}
+server_handler = {}
 
 ### PlayerQueue Class ###
-class PlayerQueue(commands.Cog):
+class PlayerQueue():
     '''Queue object to maintain queue list and timing'''
-    def __init__(self, bot):
+    def __init__(self, voice_client, voice, text):
         super().__init__()
         self.user_list = []
-        self.last_event = time.time()
-        self.voice_channel = None
-        self.text_channel = None
-
-        #self.update_queue.start()
+        self.last_event = datetime.datetime.now()
+        self.voice_client = voice_client
+        self.voice_channel = voice
+        self.text_channel = text
 
     ## Internal Functions ##
 
@@ -148,208 +145,233 @@ class PlayerQueue(commands.Cog):
             #clean up
             await msg_cleanup(self.text_channel, msgs, 5)
 
-    async def msg_cleanup(self, msgs, delay):
-        '''deletes messages after given delay'''
-        await asyncio.sleep(delay)
-        for msg in msgs:
-            await msg.delete()
-
-    ## Class Loops ##
-
-    @tasks.loop(seconds=10)
-    async def update_queue_loop(self):
-        '''checks voice channel to see if anything has changed'''
-        await self.update_queue()
-
-    ## Commands ##
-
-    @commands.command(name='create', help='Creates initial queue of users')
-    async def create_queue(self, ctx):
-        '''Creates initial queue of users'''
-        msgs = []
-
-        voice_obj = None
-
-        #checks if user is in voice chat
-        try:
-            voice_obj = ctx.message.author.voice
-            channel = ctx.author.voice.channel
-            await voice_obj.channel.connect()
-        except:
-            #remind user they are not in a voice chat room
-            msgs.append(await ctx.send('You are not in a voice channel. Please join and try again'))
-
-            #clean up
-            await self.msg_cleanup(ctx, msgs, 5)
-            return
-
-        #checks if a queue exists for the current voice channel
-        '''
-        if voice_obj.channel.id in server_handler.keys():
-            msgs.append(ctx.send(f'Voice channel already has a queue. Please end before re-creating.'))
-            await self.msg_cleanup(ctx, msgs, 5)
-            return'''
-
-        self.voice_channel = voice_obj.channel
-        self.text_channel = ctx.message.channel
-
-        for user in channel.members:
-            if user.bot:
-                pass
-                #await ctx.send(f'Cannot add {user.display_name} since they are a bot')
-            else:
-                await self.append_user(user)
-
-        await self.shuffle_queue()
-
-        #create queue and list
-        name_string = self.user_list[0].mention
-        await self.text_channel.send(f'Queue Created! First up is {name_string}')
-        #await self.print_queue()
-
-        #start loops
-        await self.update_queue_loop.start()
-
-        #clean up
-        await self.msg_cleanup(msgs, 5)
-        
-    @commands.command(name='next', help='Moves to next person in the queue')
-    async def next_up(self, ctx):
-        '''moves to the next person in the queue'''
-        msgs = []
-
-        #gets new leader and mentions them
-        name_string = await self.next_user()
-        msgs.append(await self.text_channel.send(f'Next person is {name_string} and the queue is as follows:'))
-        await self.print_queue()
-
-        #clean up
-        await self.msg_cleanup(msgs, 5)
-
-    @commands.command(name='add', help='Adds a person to the queue')
-    async def add(self, ctx):
-        '''adds a specific person to the queue'''
-        msgs = []
-
-        #gets mentions
-        mentions = ctx.message.mentions
-
-        #containers
-        added_string = 'Added '
-        bot_msg = None
-
-        if len(mentions) == 0:
-            #if no mentions cannot remove, post error to chat
-            msgs.append(await self.text_channel.send('Nobody added! Make sure to use the @ symbol when adding or removing'))
-        elif len(mentions) == 1 and mentions[0].bot:
-            #if the mention is a bot throw and error
-            msgs.append(await self.text_channel.send(f'Cannot add {mentions[0].display_name} because they are a bot'))
-        else:
-            #add user to queue
-            for i, user in enumerate(mentions):
-                if user.bot:
-                    msgs.append(await self.text_channel.send(f'Cannot add {user.display_name} because they are a bot'))
-                else:
-                    await self.append_user(user)
-                    added_string = added_string + f'{user.display_name}'
-
-                    if i+1 < len(mentions):
-                        added_string = added_string + ', '
-
-            await self.text_channel.send(f'{added_string}')
-            await self.print_queue()
-
-        #clean up bot messages
-        if bot_msg != None:
-            await self.msg_cleanup(msgs, 5)
-
-    @commands.command(name='remove', help='Removes a specific person from the queue')
-    async def remove(self, ctx, person):
-        '''removes specific person from queue'''
-        msgs = []
-
-        #get person to remove
-        mentions = ctx.message.mentions
-
-        #containers
-        removed_list = ''
-
-        if len(mentions) == 0:
-            #if no mentions, cannot remove
-            msgs.append(await self.text_channel.send('Nobody removed! Make sure to use the @ symbol when adding or removing'))
-        else:
-            #remove listed mentions
-            for user in mentions:
-                if user in await self.current_queue():
-                    await self.remove_user(user)
-                    removed_list += user.display_name
-
-            await self.text_channel.send(f'Removed {removed_list}')
-        
-        #prints current queue
-        await self.print_queue()
-
-        #clean up bot messages
-        if len(msgs) != 0:
-            await self.msg_cleanup(msgs, 5)
-
-    @commands.command(name='queue', help='Displays current queue')
-    async def queue(self, ctx):
-        '''Displays a message with the current queue'''
-        await self.print_queue()
-
-    @commands.command(name='update', help='Updates queue with new users automatically')
-    async def force_update(self, ctx):
-        '''updates queue automatically'''
-        await self.update_queue()
-
-    @commands.command(name='shuffle', help='Reshuffles current queue')
-    async def shuffle(self, ctx):
-        '''reshuffles current queue list'''
-        msgs = []
-
-        await self.shuffle_queue()
-
-        msgs.append(await self.text_channel.send(f'Queue Shuffled!'))
-        await self.print_queue()
-        await self.msg_cleanup(msgs, 5)
-
-    @commands.command(name='end', help='Ends current queue and disconnects from voice chat')
-    async def end(self, ctx):
-        '''ends queue and disconnects'''
-        #del server_handler[ctx.message.author.voice.channel.id]
-        msgs = []
-
-        self.user_list = []
-
-        #disconnects from voice chat
-        server = ctx.message.guild.voice_client
-        await server.disconnect()
-        msgs.append(await self.text_channel.send('Ended queue and disconnected. See you next time!'))
-
-        #clean up
-        await self.msg_cleanup(msgs, 5)
-
-
 ### Helper Functions ###
 
-'''
-@tasks.loop(seconds=120)
-async def check_timer():
-    #checks to see if queues have been running without being modified
-    global server_handler
+@tasks.loop(seconds=10)
+async def queue_prune():
+    '''prunes old queues'''
+    key_holder = []
+    if len(server_handler) != 0:
+        for key in server_handler:
+            time_diff =  datetime.datetime.now() - server_handler[key].last_event
+            if time_diff.total_seconds() > 60*60*1: #1 hour
+                msgs = []
 
-    for key in server_handler.keys():
-        if server_handler[key].check_time() == True:
-            del server_handler[key]'''
+                #disconnects from voice chat
+                server = server_handler[key].voice_client
+                await server.disconnect()
+                text_channel = server_handler[key].text_channel
+                msgs.append(await text_channel.send('Queue timed out, ended queue and disconnected. See you next time!'))
 
+                key_holder.append(key)
 
-@client.event
+                #clean up
+                await msg_cleanup(msgs, 5)
+
+    for key in key_holder:
+        del server_handler[key] 
+
+    activity = discord.Activity(type= discord.ActivityType.listening, name=f"to {len(server_handler)} queues")
+    await bot.change_presence(status=discord.Status.online, activity=activity)
+
+async def get_user_list(ctx):
+    '''returns PlayerQueue for voice channel or new PlayerQueue if new'''
+    if server_handler.get(ctx.message.author.voice.channel.id) == None:
+        new_queue = PlayerQueue(ctx.message.guild.voice_client, ctx.message.author.voice.channel, ctx.message.channel)
+        server_handler[ctx.message.author.voice.channel.id] = new_queue
+        return new_queue
+    else:
+        return server_handler.get(ctx.message.author.voice.channel.id)
+
+async def msg_cleanup(msgs, delay):
+    '''deletes messages after given delay'''
+    await asyncio.sleep(delay)
+    for msg in msgs:
+        await msg.delete()
+
+### Bot Functions ###
+
+@bot.command(name='create', help='Creates initial queue of users')
+async def create_queue(ctx):
+    '''Creates initial queue of users'''
+    msgs = []
+
+    #checks if user is in voice chat
+    try:
+        voice_obj = ctx.message.author.voice
+        channel = ctx.author.voice.channel
+        try:
+            await channel.connect()
+        except:
+            pass
+        
+    except Exception as e:
+        print(str(e))
+        #remind user they are not in a voice chat room
+        msgs.append(await ctx.send('You are not in a voice channel. Please join and try again'))
+
+        #clean up
+        await msg_cleanup(msgs, 5)
+        return
+
+    #get user list 
+    user_queue = await get_user_list(ctx)
+
+    for user in user_queue.voice_channel.members:
+        print(user.name)
+        if user.bot:
+            pass
+            #await ctx.send(f'Cannot add {user.display_name} since they are a bot')
+        else:
+            await user_queue.append_user(user)
+
+    await user_queue.shuffle_queue()
+
+    #create queue and list
+    name_string = user_queue.user_list[0].mention
+    await user_queue.text_channel.send(f'Queue Created! First up is {name_string}')
+    await user_queue.print_queue()
+
+    #start loops
+    #await user_queue.update_queue_loop.start()
+
+    #clean up
+    await msg_cleanup(msgs, 5)
+    
+@bot.command(name='next', help='Moves to next person in the queue')
+async def next_up(ctx):
+    '''moves to the next person in the queue'''
+    msgs = []
+    user_queue = await get_user_list(ctx)
+
+    #gets new leader and mentions them
+    name_string = await user_queue.next_user()
+    msgs.append(await user_queue.text_channel.send(f'Next person is {name_string} and the queue is as follows:'))
+    await user_queue.print_queue()
+    user_queue.last_event = datetime.datetime.now()
+
+    #clean up
+    await msg_cleanup(msgs, 5)
+
+@bot.command(name='add', help='Adds a person to the queue')
+async def add(ctx):
+    '''adds a specific person to the queue'''
+    msgs = []
+    user_queue = await get_user_list(ctx)
+
+    #gets mentions
+    mentions = ctx.message.mentions
+
+    #containers
+    added_string = 'Added '
+    bot_msg = None
+
+    if len(mentions) == 0:
+        #if no mentions cannot remove, post error to chat
+        msgs.append(await user_queue.text_channel.send('Nobody added! Make sure to use the @ symbol when adding or removing'))
+    elif len(mentions) == 1 and mentions[0].bot:
+        #if the mention is a bot throw and error
+        msgs.append(await user_queue.text_channel.send(f'Cannot add {mentions[0].display_name} because they are a bot'))
+    else:
+        #add user to queue
+        for i, user in enumerate(mentions):
+            if user.bot:
+                msgs.append(await user_queue.text_channel.send(f'Cannot add {user.display_name} because they are a bot'))
+            else:
+                await user_queue.append_user(user)
+                added_string = added_string + f'{user.display_name}'
+
+                if i+1 < len(mentions):
+                    added_string = added_string + ', '
+
+        await user_queue.text_channel.send(f'{added_string}')
+        await user_queue.print_queue()
+        user_queue.last_event = datetime.datetime.now()
+
+    #clean up bot messages
+    if bot_msg != None:
+        await msg_cleanup(msgs, 5)
+
+@bot.command(name='remove', help='Removes a specific person from the queue')
+async def remove(ctx, person):
+    '''removes specific person from queue'''
+    msgs = []
+    user_queue = await get_user_list(ctx)
+
+    #get person to remove
+    mentions = ctx.message.mentions
+
+    #containers
+    removed_list = ''
+
+    if len(mentions) == 0:
+        #if no mentions, cannot remove
+        msgs.append(await user_queue.text_channel.send('Nobody removed! Make sure to use the @ symbol when adding or removing'))
+    else:
+        #remove listed mentions
+        for user in mentions:
+            if user in await user_queue.current_queue():
+                await user_queue.remove_user(user)
+                removed_list += user.display_name
+
+        await user_queue.text_channel.send(f'Removed {removed_list}')
+    
+    #prints current queue
+    await user_queue.print_queue()
+    user_queue.last_event = datetime.datetime.now()
+
+    #clean up bot messages
+    if len(msgs) != 0:
+        await msg_cleanup(msgs, 5)
+
+@bot.command(name='queue', help='Displays current queue')
+async def queue(ctx):
+    '''Displays a message with the current queue'''
+    user_queue = await get_user_list(ctx)
+    await user_queue.print_queue()
+    user_queue.last_event = datetime.datetime.now()
+
+@bot.command(name='update', help='Updates queue with new users automatically')
+async def force_update(ctx):
+    '''updates queue automatically'''
+    user_queue = await get_user_list(ctx)
+    await user_queue.update_queue()
+    user_queue.last_event = datetime.datetime.now()
+
+@bot.command(name='shuffle', help='Reshuffles current queue')
+async def shuffle(ctx):
+    '''reshuffles current queue list'''
+    msgs = []
+    user_queue = await get_user_list(ctx)
+
+    await user_queue.shuffle_queue()
+
+    msgs.append(await user_queue.text_channel.send(f'Queue Shuffled!'))
+    await user_queue.print_queue()
+    user_queue.last_event = datetime.datetime.now()
+    await msg_cleanup(msgs, 5)
+
+@bot.command(name='end', help='Ends current queue and disconnects from voice chat')
+async def end(ctx):
+    '''ends queue and disconnects'''
+    msgs = []
+    del server_handler[ctx.message.author.voice.channel.id] 
+
+    #disconnects from voice chat
+    server = ctx.message.guild.voice_client
+    await server.disconnect()
+    msgs.append(await ctx.send('Ended queue and disconnected. See you next time!'))
+
+    #clean up
+    await msg_cleanup(msgs, 5)
+
+@bot.event
 async def on_ready():
     '''Prints message on server connection'''
-    print(f'{client.user} has connected to Discord!')
-    check_timer.start()
+    print(f'\n{bot.user.name} has connected to Discord!\n')
+    activity = discord.Activity(type= discord.ActivityType.listening, name=f"to {len(server_handler)} queues")
+    await bot.change_presence(status=discord.Status.online, activity=activity)
+    await queue_prune.start()
 
-bot.add_cog(PlayerQueue(bot))
 bot.run(TOKEN)
 
